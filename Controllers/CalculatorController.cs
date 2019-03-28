72e60ce8aa8651d4117491dab32bc43bcb20ad09
sakiry1 <38807848+sakiry1@users.noreply.github.com>
2019-03-28 17:55:22 +0100

    Add files via upload

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("Calculator")]
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index(double num1, double num2)
+        {
+            var clase = new CalculatorClass();
+
+            return View(clase);
+            //return View(new CalculatorClass()); //son lo mismo
+        }
+
+        // POST: Calculator/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CalculatorClass calculator)
+        {
+            var n = calculator.Valor1.GetType();
+            var n1 = calculator.Valor2.GetType();
+            var op = calculator.Operador.GetType();
+            
+            try
+            {
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Models/CalculatorClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class CalculatorClass
+    {
+        public double Valor1 { get; set;}
+        public double Valor2 { get; set; }
+        public string Operador { get; set; }
+        public double Resultado { get; set; }
+
+    }
+}
